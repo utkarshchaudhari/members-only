afaab99a6292c383b8a5bfa726159a8dca0b700d
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-10-07 19:39:15 -0400

    Setup controller for user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 //Handle User create on POST
 exports.user_create_post = [
@@ -53,4 +54,44 @@ exports.user_create_post = [
       res.sendStatus(200);
     }
   }),
+];
+
+exports.user_login_post = [
+  body('email')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('E-mail must be specified.')
+    .isEmail()
+    .withMessage('Enter a valid email.'),
+  body('password')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Password must be specified.'),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ error: errors.mapped() });
+    }
+
+    passport.authenticate('local', (err, user, info) => {
+      console.log(err, user, info);
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(401).json({ err: 'Incorrect email or password.' });
+      }
+
+      req.login(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+
+        return res.json({ message: 'Logged In Successfully!', user });
+      });
+    })(req, res, next);
+  },
 ];
