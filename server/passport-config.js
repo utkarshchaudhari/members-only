868d4f4833a42eb58cc18927e9379669f3361cc2
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-10-07 19:02:31 -0400

    Configured Passport LocalStrategy

---
+++ server/passport-config.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+const bcrypt = require('bcryptjs');
+
+//setting up the LocalStrategy
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+    },
+    async (username, password, done) => {
+      try {
+        const user = await User.findOne({ email: username });
+        const passwordMatch =
+          user && (await bcrypt.compare(password, user.password));
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username' });
+        }
+        if (!passwordMatch) {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
