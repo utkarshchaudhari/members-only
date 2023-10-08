e99ae4aadc018d2de11ecd8db39fa63ef037c68f
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-10-08 00:46:18 -0400

    Updated Header buttons on login

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import SignupModal from './ui/SignupModal';
 import LoginModal from './ui/LoginModal';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   const [signupModal, setSignupModal] = useState(false);
   const [loginModal, setLoginModal] = useState(false);
+  const [user, setUser] = useState(null);
 
   return (
     <header>
@@ -13,18 +16,44 @@ function Header() {
           <span className="indigo">Members</span>Only
         </h1>
         <div className="header_buttons">
-          <button className="button" onClick={() => setLoginModal(!loginModal)}>
-            Log In
-          </button>
-          <button
-            className="button sign-up_btn"
-            onClick={() => setSignupModal(!signupModal)}
-          >
-            Sign Up
-          </button>
+          {user ? (
+            <>
+              <button className="button">Join Club</button>
+              <button className="button btn_margin-left">New Message</button>
+              <button className="account_btn btn_margin-left">
+                Account
+                <FontAwesomeIcon
+                  icon={faCaretDown}
+                  size="lg"
+                  style={{ color: '#9ea3af', marginLeft: '10px' }}
+                />
+              </button>
+            </>
+          ) : (
+            <>
+              <button
+                className="button"
+                onClick={() => setLoginModal(!loginModal)}
+              >
+                Log In
+              </button>
+              <button
+                className="button btn_margin-left"
+                onClick={() => setSignupModal(!signupModal)}
+              >
+                Sign Up
+              </button>
+            </>
+          )}
         </div>
       </div>
-      {loginModal && <LoginModal setLoginModal={setLoginModal} />}
+      {loginModal && (
+        <LoginModal
+          setLoginModal={setLoginModal}
+          user={user}
+          setUser={setUser}
+        />
+      )}
       {signupModal && <SignupModal setSignupModal={setSignupModal} />}
     </header>
   );
