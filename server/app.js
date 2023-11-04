251de18c1f82012a4ef945a4c9f3a80a1e22399c
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-11-04 18:12:58 -0400

    Configured MongoStore for Sessions

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const passport = require('passport');
 const cors = require('cors');
 const path = require('path');
@@ -8,7 +9,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
 dotenv.config();
-require('./passport-config');
 
 const indexRouter = require('./routes/index');
 
@@ -24,11 +24,11 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(process.env.SESSION_KEY));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Session middleware
@@ -37,10 +37,12 @@ app.use(
     secret: process.env.SESSION_KEY,
     resave: false,
     saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URL }),
   })
 );
 
 //Passport middleware
+require('./passport-config');
 app.use(passport.initialize());
 app.use(passport.session());
 
