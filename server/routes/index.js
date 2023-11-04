190d03b05a96a5825f1d8d56a6023ba430ba9fc5
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-11-04 18:25:07 -0400

    Setup route for new message

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 //Require controller modules
 const user_controller = require('../controllers/userController');
+const message_controller = require('../controllers/messageController');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -12,7 +13,10 @@ router.get('/', function (req, res, next) {
 //POST request for creating user
 router.post('/signup', user_controller.user_create_post);
 
-//POST reqyest for log-in
+//POST request for log-in
 router.post('/login', user_controller.user_login_post);
+
+//POST request for new message
+router.post('/newmessage', message_controller.new_message_post);
 
 module.exports = router;
