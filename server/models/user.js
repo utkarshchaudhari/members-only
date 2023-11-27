915835a37d9d0e99a483d6fd84c171e0f69670da
utkarshchaudhari <utkarshchaudhari1996@gmail.com>
2023-11-26 21:31:21 -0500

    Updated user model schema

--- server/models/user.js
+++ server/models/user.js
@@ -6,6 +6,7 @@ const userSchema = new Schema({
   name: { type: String, required: true, maxLength: 100 },
   email: { type: String, required: true, maxLength: 50 },
   password: { type: String, required: true, minLength: 8 },
+  member: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('User', userSchema);
